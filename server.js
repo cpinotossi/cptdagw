5188071915cc0a5f2fc04e16d3b7f72745249857
cpinotossi <cpinotossi@web.de>
2021-12-15 20:10:10 +0100

    add http support

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env nodejs
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 
-const port = 8080
-
+const portSSL = 443
 const options = {
   key: fs.readFileSync('./openssl/svr.key'),
   cert: fs.readFileSync('./openssl/svr.crt')
 };
+var serverSSL = https.createServer(options);
+serverSSL.on('request',(req,res)=>{
+    res.write(JSON.stringify(req.headers, null, '\t'));
+    res.end();
+});
+serverSSL.listen(portSSL,()=>{
+    console.log(`Server waiting on port ${portSSL} for you`)
+})
 
-var server = https.createServer(options);
+const port = 80
+var server = http.createServer();
 server.on('request',(req,res)=>{
     res.write(JSON.stringify(req.headers, null, '\t'));
     res.end();
